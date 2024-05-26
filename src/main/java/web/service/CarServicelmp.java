d93fd87e821dfddacb493e6404e8962102b2f96d
Дмитрий Сотников <DIMA10299@mail.ru>
2024-05-26 18:05:10 +0300

    Выполнил!

---
+++ src/main/java/web/service/CarServicelmp.java
@@ -0,0 +1,30 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CarServicelmp implements CarService{
+    private List<Car> carList = Arrays.asList(
+            new Car("BMW", "m5", 2000),
+            new Car("Reno", "Logan", 2010),
+            new Car("Lexus", "CLX", 2015),
+            new Car("Lada", "Priora", 2018),
+            new Car("Moscvich", "standart", 2020)
+    );
+
+    public List<Car> getAllCars() {
+        return carList;
+    }
+
+    public List<Car> getCarsByCount(int count) {
+        if (count >= 5) {
+            return carList;
+        } else {
+            return carList.subList(0, count);
+        }
+    }
+}
